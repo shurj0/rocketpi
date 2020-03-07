8f6852c830549b24f8cd76274423c3c6e708dab7
shurjo <sakibmas@msu.edu>
2020-03-07 00:41:58 -0500

    initial commit

---
+++ Rocket.py
@@ -0,0 +1,69 @@
+import gpiozero
+from math import log
+from time import sleep
+
+class Rocket:
+    def __init__(self, mass, cd, motor_right_pin, motor_left_pin, solenoid_pin):
+        self.mass = mass
+        self.cd = cd
+        self.motor_right = gpiozero.DigitalOutputDevice(motor_right_pin)
+        self.motor_left = gpiozero.DigitalOutputDevice(motor_left_pin)
+        self.solenoid = gpiozero.DigitalOutputDevice(solenoid_pin)
+        self.main_valve_open = False
+        self.fill_drain_open = False
+
+    # Define the control valve opening/closing functions for easier use and state checking.
+    # Assumes control valve is closed at startup, since the wires being used with the driver
+    # can't be used to check position of the stepper motor. 
+
+    def open_main(self):
+        self.motor_right.on()
+        sleep(0.5)
+        self.motor_right.off()
+        self.main_valve_open = True
+
+    
+    def close_main(self):
+        self.motor_left.on()
+        sleep(0.5)
+        self.motor_left.off()
+        self.main_valve_open = False
+
+    # MUST check that control valve is closed before opening fill/drain valve
+
+    def open_fill_drain(self):
+        if self.main_valve_open:
+            self.close_main()
+        self.solenoid.on()
+        self.fill_drain_open = True
+    
+    def close_fill_drain(self):
+        self.solenoid.off()
+        self.fill_drain_open = False
+
+    # Setters are used instead of manually changing class attributes
+
+    def set_position(self, x):
+        self.x = x
+    
+    def set_velocity(self, v):
+        self.v = v
+    
+    def set_acceleration(self, a):
+        self.a = a
+    
+    # Apogee calculator
+
+    def predApogee(self, current_velocity):
+        # Rename variables for cleaner calculation
+        m = self.mass
+        C = self.cd
+        g = 9.81
+
+        intermediate = 1 + (C * (current_velocity ** 2)) / (m * g)
+        max_height = (m / (2*C)) * log(intermediate)
+
+        apogee = self.x + max_height
+
+        return apogee
+    
---
+++ main.py
@@ -0,0 +1,62 @@
+import gpiozero
+from time import sleep
+from Rocket import Rocket
+
+# Units are in metric, need to convert to imperial probably
+
+mass = 45 
+cd = 0.05
+target_apogee = 3300
+
+# Create an instance of the rocket object, and set pins
+# motor_right_pin = GPIO17
+# motor_left_pin = GPIO18
+# solenoid_pin = GPIO22
+
+rocket = Rocket(mass, cd, 17, 18, 22)
+
+# TODO: Define the sensor reader function here
+# The function should return the values in the form of a tuple
+# (altitude, velocity, acceleration)
+
+def sensorReader():
+    return (0,0,0)
+
+def launch(target_apogee):
+    if rocket.fill_drain_open:
+        rocket.close_fill_drain()
+    # Countdown
+    sleep(10)
+    rocket.open_main()
+    print("Control valve opened.")
+
+    # Sets the altitude, velocity and acceleration in the rocket object and runs the predApogee() function. 
+    # If the apogee returned by that function is greater than the target_apogee, the main valve is closed, and the loop is exited.
+    # For optimal results, undervalue the target_apogee by around 300, as the algorithm tends to overshoot a little by around that amount
+
+    while rocket.main_valve_open:
+        sensor_values = sensorReader()
+        rocket.set_position(sensor_values[0])
+        rocket.set_velocity(sensor_values[1])
+        rocket.set_acceleration(sensor_values[2])
+        predicted_apogee = rocket.predApogee(rocket.v)
+        if predicted_apogee > target_apogee:
+            rocket.close_main()
+
+    return predicted_apogee
+
+while True:
+    command = input("Command: ")
+    if command == "openmain":
+        rocket.open_main()
+    if command == "closemain":
+        rocket.close_main()
+    if command == "openfill":
+        rocket.open_fill_drain()
+    if command == "closefill":
+        rocket.close_fill_drain()
+    if command == "launch":
+        print("Initiating launch sequence...")
+        final_prediction = launch(target_apogee)
+        print("Shutting down engine. Rocket should reach an apogee of {}.".format(final_prediction))
+        break
